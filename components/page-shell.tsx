8bc469f3d34e8204e5d5630304f1567a4565488f
Zach Sents <zachsents@gmail.com>
2024-03-06 06:22:07 -0700

    Added favicon

--- components/page-shell.tsx
+++ components/page-shell.tsx
@@ -1,5 +1,8 @@
 import { cgen } from "../server-modules/util"
 
+const headline = "Dollar Waitlist"
+const description = "Build premium waitlists"
+
 export default function PageShell({ children, title, afterBody, scriptName, ...props }: PageShellProps) {
     return (<>
         {"<!DOCTYPE html>"}
@@ -14,6 +17,24 @@ export default function PageShell({ children, title, afterBody, scriptName, ...p
                 <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet" />
 
                 <meta name="htmx-config" content='{"includeIndicatorStyles": false}' />
+
+                {/* Favicon & other icons */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
+                <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+                <link rel="manifest" href="/assets/site.webmanifest" />
+
+                {/* OpenGraph */}
+                <meta
+                    property="og:title"
+                    content={headline}
+                />
+                <meta
+                    property="og:description"
+                    content={description}
+                />
+                <meta property="og:image" content="/assets/og.png" />
             </head>
 
             <body
