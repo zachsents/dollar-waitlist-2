dc3c19d9fc91ebfe5ca99404b91494bf98ff52cf
Zach Sents <zachsents@gmail.com>
2024-03-06 04:28:18 -0700

    Added port from env

--- index.ts
+++ index.ts
@@ -385,7 +385,7 @@ app.get("/*", renderPage())
 /*                                  App Setup                                 */
 /* -------------------------------------------------------------------------- */
 
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
